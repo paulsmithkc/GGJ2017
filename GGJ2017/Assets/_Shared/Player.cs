6ddc15a600978d71cff2ffabcf9fac7501c53eb2
Cole Alves <cole101013@yahoo.com>
2017-01-21 12:51:00 -0600

    Energy Recovery

--- GGJ2017/Assets/_Shared/Player.cs
+++ GGJ2017/Assets/_Shared/Player.cs
@@ -9,19 +9,27 @@ public class Player : MonoBehaviour {
     public int CurrentEnergy = 100;
     public int MaxEnergy = 100;
     public bool Dead = false;
-    
+    public float EnergyRecoveryDelay = 1;
+    private float EnergyRecoveryLeft = 0;
 	
 	// Use this for initialization
 	void Start () {
 		CurrentHealth = MaxHealth;
         CurrentEnergy = MaxEnergy;
-		Dead = false; 
+		Dead = false;
+        EnergyRecoveryLeft = EnergyRecoveryDelay;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float deltaTime = Time.deltaTime;
+        EnergyRecoveryLeft -= deltaTime;
+        if (EnergyRecoveryLeft <= 0)
+        {
+            EnergyRecoveryLeft += EnergyRecoveryDelay;
+            AddEnergy(1);
+        }
+    }
 	
 	public void AddHealth(int value)
 	{
@@ -49,5 +57,7 @@ public class Player : MonoBehaviour {
         {
             MaxEnergy = 0;
         }
+
+        
     }
 }
