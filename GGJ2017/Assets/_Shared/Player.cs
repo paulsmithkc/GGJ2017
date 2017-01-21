23f4c606f9061fe50809fe8140866d88b5fc1ead
Cole Alves <cole101013@yahoo.com>
2017-01-20 23:20:02 -0600

    Adding player health

---
+++ GGJ2017/Assets/_Shared/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public int CurrentHealth = 100;
+	public int MaxHealth = 100;
+	public bool Dead = false;
+	
+	// Use this for initialization
+	void Start () {
+		CurrentHealth = MaxHealth;
+		Dead = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void AddHealth(int value)
+	{
+		CurrentHealth += value;
+		if(CurrentHealth > MaxHealth)
+		{
+			CurrentHealth = MaxHealth;
+		}
+		if(CurrentHealth <= 0)
+		{
+			Dead = true;
+		}
+	}
+}
