fbba77bf724f009435d70d1fc874b41abac769aa
Cole Alves <cole101013@yahoo.com>
2017-01-20 22:48:10 -0600

    enemy markers

--- GGJ2017/Assets/Map/GameMap.cs
+++ GGJ2017/Assets/Map/GameMap.cs
@@ -25,6 +25,54 @@ public class GameMap : MonoBehaviour {
         {
             _gestureDetector.pinch += OnPinch;
         }
+		
+		_googleMap._markers = new GoogleMapMarker[4];
+		_googleMap._markers[0] = new GoogleMapMarker
+		{
+			size = GoogleMapMarkerSize.mid,
+			color = GoogleMapColor.blue,
+			label = "1",
+			location = new GoogleMapLocation
+			{
+				latitude = 38.711320f, 
+				longitude = -90.311533f
+			}
+		};
+		_googleMap._markers[1] = new GoogleMapMarker
+		{
+			size = GoogleMapMarkerSize.mid,
+			color = GoogleMapColor.blue,
+			label = "2",
+			location = new GoogleMapLocation
+			{
+				latitude = 38.711167f, 
+				longitude = -90.311361f
+			}
+		};
+		
+		_googleMap._markers[2] = new GoogleMapMarker
+		{
+			size = GoogleMapMarkerSize.mid,
+			color = GoogleMapColor.blue,
+			label = "3",
+			location = new GoogleMapLocation
+			{
+				latitude = 38.710472f, 
+				longitude = -90.311194f
+			}
+		};
+		
+		_googleMap._markers[3] = new GoogleMapMarker
+		{
+			size = GoogleMapMarkerSize.mid,
+			color = GoogleMapColor.blue,
+			label = "4",
+			location = new GoogleMapLocation
+			{
+				latitude = 38.709778f, 
+				longitude = -90.311778f
+			}
+		};
     }
 	
 	// Update is called once per frame
